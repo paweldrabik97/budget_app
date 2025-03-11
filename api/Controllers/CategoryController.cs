88d04341a597640f33ce8203735591d1f6f69ab7
paweldrabik97 <paweldrabik97@gmail.com>
2025-03-11 20:43:19 +0100

    Add Category and TransactionType controllers with CRUD operations

---
+++ api/Controllers/CategoryController.cs
@@ -0,0 +1,63 @@
+﻿using api.DTOs.Category;
+using api.Interfaces;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepo;
+        public CategoryController(ICategoryRepository categoryRepo)
+        {
+            _categoryRepo = categoryRepo;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetCategories()
+        {
+            var categories = await _categoryRepo.GetCategoriesAsync();
+            return Ok(categories);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategoryById([FromRoute] int id)
+        {
+            var category = await _categoryRepo.GetCategoryByIdAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category.ToDto());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateCategoryRequestDto categoryDto)
+        {
+            var category = categoryDto.ToCategoryFromCreateDto();
+            await _categoryRepo.CreateCategoryAsync(category);
+            return CreatedAtAction(nameof(GetCategoryById), new { id = category.Id }, category.ToDto());
+        }
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCategoryRequestDto categoryDto)
+        {
+            var category = await _categoryRepo.UpdateCategoryAsync(id, categoryDto);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category.ToDto());
+        }
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var category = await _categoryRepo.DeleteCategoryAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category.ToDto());
+        }
+    }
+}
---
+++ api/Controllers/TransactionTypeController.cs
@@ -0,0 +1,63 @@
+﻿using api.DTOs.TransactionType;
+using api.Interfaces;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionTypeController : ControllerBase
+    {
+        private readonly ITransactionTypeRepository _transactionTypeRepo;
+        public TransactionTypeController(ITransactionTypeRepository transactionTypeRepo)
+        {
+            _transactionTypeRepo = transactionTypeRepo;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetTransactionTypes()
+        {
+            var transactionTypes = await _transactionTypeRepo.GetTransactionTypesAsync();
+            return Ok(transactionTypes);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTransactionTypeById([FromRoute] int id)
+        {
+            var transactionType = await _transactionTypeRepo.GetTransactionTypeByIdAsync(id);
+            if (transactionType == null)
+            {
+                return NotFound();
+            }
+            return Ok(transactionType.ToDto());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateTransactionTypeRequestDto transactionTypeDto)
+        {
+            var transactionType = transactionTypeDto.ToTransactionTypeFromCreateDto();
+            await _transactionTypeRepo.CreateTransactionTypeAsync(transactionType);
+            return CreatedAtAction(nameof(GetTransactionTypeById), new { id = transactionType.Id }, transactionType.ToDto());
+        }
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateTransactionTypeRequestDto transactionTypeDto)
+        {
+            var transactionType = await _transactionTypeRepo.UpdateTransactionTypeAsync(id, transactionTypeDto);
+            if (transactionType == null)
+            {
+                return NotFound();
+            }
+            return Ok(transactionType.ToDto());
+        }
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var transactionType = await _transactionTypeRepo.DeleteTransactionTypeAsync(id);
+            if (transactionType == null)
+            {
+                return NotFound();
+            }
+            return Ok(transactionType.ToDto());
+        }
+    }
+}
